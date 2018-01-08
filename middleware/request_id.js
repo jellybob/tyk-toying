36676b8d2d68713ea290baf51703782f554e8a26
Nick Bolt <nickbolt@Nick-Bolt.local>
2018-01-08 14:51:07 +0000

    Add request id in middleware
    A request now has a unique tyk-content-request-id which is truely unique
    to every request.  There is also a request-id which you can pass
    through.  If blank the request-id will default to the
    tyk-content-request-id (as a generation method on the initial request).
    
    Have had to add tyk.js, which is apparently not needed so should remove
    this in the future. (see
    https://github.com/TykTechnologies/tyk/commit/0f5ab6ce9500efb6845749f0abe5acdfaa25f2fd)

--- middleware/request_id.js
+++ middleware/request_id.js
@@ -1,11 +1,10 @@
 var request_id = new TykJS.TykMiddleware.NewMiddleware({});
 
-// Initialise it with your functionality by passing a closure that accepts two objects
-// into the NewProcessRequest() function:
 request_id.NewProcessRequest(function(request, session) {
-
-  console.log("This middleware does nothing, but will print this to your terminal.")
-
-  // You MUST return both the request and session metadata
+  var tykContentRequestId = request.Headers["Tyk-Content-Request-Id"][0];
+  var requestId = request.Headers["Request-Id"];
+  if (requestId === undefined) {
+    request.SetHeaders["Request-Id"] = tykContentRequestId;
+  }
   return request_id.ReturnData(request, session.meta_data);
 });
